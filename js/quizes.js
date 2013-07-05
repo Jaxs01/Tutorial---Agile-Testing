ff4b39db160d00ed2245f6ebf178ba9a5081cba1
Todd Brackley <todd@goneopen.com>
2013-07-05 17:24:41 +1200

    Testing Zealandia - agile part of the exercise

--- js/quizes.js
+++ js/quizes.js
@@ -9,7 +9,7 @@ angular.module('ng').
       { answer: "b", question: "Test-infected developers, better tools, better-designed tests"}
     ];
   })
-  .controller('TestingZoo', function($scope){
+  .controller('TestingZoo', function ($scope) {
     $scope.title = "How does agile change things?";
     $scope.options = { a: "Waterfall", b: "Agile" };
     $scope.questions = [
@@ -17,5 +17,22 @@ angular.module('ng').
       { answer: "a", question: "Testing does not need a requirements document", hint: "Both waterfall and agile needs requirements - waterfall has then all upfront whereas agile has then fully for only that small piece of development"},
       { answer: "b", question: "Testing occurs in small timeframes within development cycles", hint: "Testing is squeezed into small cycles such that testing and development work together"},
       { answer: "b", question: "Test-infected developers, collaboration between testers and developers", hint: "Testing avoids being done in isolation. Testers know what was the intended functionality because they helped with the design."}
+    ];
+  })
+  /*
+   * Opportunity to ask questions early
+   * Collaborate with developers and business
+   * Fewer bugs
+   * Better quality product
+   */
+  .controller('Zealandia', function ($scope) {
+    $scope.title = "How does agile change things?";
+    $scope.options = { a: "Waterfall", b: "Agile" };
+    $scope.questions = [
+      { answer: "b", question: "There is opportunity to ask questions early", hint: "In waterfall, testing is started after development is complete."},
+      { answer: "b", question: "Test-infected developers, collaboration between testers and developers", hint: "Testing avoids being done in isolation. Testers know what was the intended functionality because they helped with the design."},
+      { answer: "a", question: "Loads of bugs!", hint: "Both waterfall and agile have bugs - but usually in agile we are working together, getting feedback so the cost to fix is much lower - really, in agile we tend to see much lower levels of bugs overall and especially going out to the customer."},
+      { answer: "b", question: "Better quality product", hint: "If the person wanting the software is working with you throughout and providing feedback and there's testing and discussion, it's just got to be better. Don't you think?"}
+
     ];
   });
